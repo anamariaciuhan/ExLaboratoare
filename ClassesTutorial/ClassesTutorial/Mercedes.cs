09a9e1621e66e0db5f0f7b2bff25f5614c2af312
anamariaciuhan <anamaria.ciuhan@gmail.com>
2016-11-29 00:36:47 +0200

    ex 8 mvc course2

--- ClassesTutorial/ClassesTutorial/Mercedes.cs
+++ ClassesTutorial/ClassesTutorial/Mercedes.cs
@@ -8,8 +8,12 @@ namespace ClassesTutorial
 {
     public class Mercedes: AbstractCar // ex 2 mvc course2, ex 4 course2, ex 6
     {
+        public string Description;
+        public int Price;
         public Mercedes() // ex7 mvc course2
        {
+            Description = "EClass";
+            Price=67000;
         }
     }
 }
