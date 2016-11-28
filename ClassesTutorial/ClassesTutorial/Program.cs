82fff108bfc3916b8fcb363a85f58e5e563b59e1
anamariaciuhan <anamaria.ciuhan@gmail.com>
2016-11-29 00:39:56 +0200

    ex 9 mvc course2

--- ClassesTutorial/ClassesTutorial/Program.cs
+++ ClassesTutorial/ClassesTutorial/Program.cs
@@ -10,12 +10,14 @@ namespace ClassesTutorial
     {
         static void Main(string[] args)
         {
-            Mercedes mercedes = new Mercedes(); // ex 4 mvc course2
+            Mercedes mercedes = new Mercedes(); // ex 4 mvc course2, ex 9
             //AbstractCar aCar = new AbstractCar(); ex 4 mvc course2
             EngineStruct engine = new EngineStruct(); //ex 5 mvc course2
             AbstractCar car2 = new Mercedes(); // ex 6 mvc course2
 
             Console.WriteLine("This is my first C# project"); //ex 1 mvc course2
+            Console.WriteLine("Mercede is " + mercedes.Description); //ex 9 mvc course2
+            Console.WriteLine("Mercede costs  " + mercedes.Price);// ex 9 mvc course2
             Console.ReadKey();
         }
     }
