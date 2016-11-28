12ce595444dca2ece2dba153d41ab1f578fab051
anamariaciuhan <anamaria.ciuhan@gmail.com>
2016-11-28 23:44:55 +0200

    laborator 1

--- ClassesTutorial/ClassesTutorial/Program.cs
+++ ClassesTutorial/ClassesTutorial/Program.cs
@@ -10,6 +10,9 @@ namespace ClassesTutorial
     {
         static void Main(string[] args)
         {
+            Mercedes mercedes = new Mercedes(); // ex 4 mvc course2
+            //AbstractCar aCar = new AbstractCar(); ex 4 mvc course2
+
             Console.WriteLine("This is my first C# project"); //ex 1 mvc course2
             Console.ReadKey();
         }
