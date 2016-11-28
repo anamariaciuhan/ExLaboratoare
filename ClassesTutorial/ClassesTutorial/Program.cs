15823bf21fa1d09514a835cb51a9ef3391aeaa05
anamariaciuhan <anamaria.ciuhan@gmail.com>
2016-11-28 23:22:46 +0200

    ex

---
+++ ClassesTutorial/ClassesTutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first C# project"); //ex 1 mvc course2
+            Console.ReadKey();
+        }
+    }
+}
